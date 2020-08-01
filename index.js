1fb3af4daec75a7c3d6df550a0214f69538fe0f5
Riku Nunokawa <nunorlabo@gmail.com>
2020-08-01 16:27:13 +0900

    deploy

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-(function(){
+(async function(){
     console.log("hello treasure");
+    try {
+        const localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    } catch(error){
+        alert(error);
+    }
+    const peer = new Peer({
+        key: '2c07d3f0-4c69-416d-a060-97d13a16fa06',
+        debug: 3
+    });
+
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
+    });
+
+    // 発信処理
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+    };
+  
+    // イベントリスナを設置する関数
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+            // video要素にカメラ映像をセットして再生
+            const videoElm = document.getElementById('their-video')
+            videoElm.srcObject = stream;
+            videoElm.play();
+        });
+    }
 })();
